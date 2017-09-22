8b5643f4ad88134b6e62283b34a084f92bf165f1
Granton Mchana <gmchana254@gmail.com>
2017-09-22 14:28:00 +0300

    additions

--- app/routes/index.js
+++ app/routes/index.js
@@ -1,4 +1,28 @@
 import Ember from 'ember';
 
+var questions = [{
+  id: 1,
+  question: "Hi, who are you and how may I help you?",
+  author: "Granton",
+  type: "general",
+  answer: [""],
+}, {
+  id: 2,
+  question: "Welcome this is your response please",
+  author: "anthony",
+  type: "oral",
+  answer:[""],
+}, {
+  id: 3,
+  question: "I may like to know more about it.",
+  author: "Fred",
+  type: "normal",
+  answer: [""],
+
+}];
+
 export default Ember.Route.extend({
+  model() {
+    return questions;
+  },
 });
