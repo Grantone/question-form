41d0a44276e1f4246667cc80f3caca5ea7f90cb3
Granton Mchana <gmchana254@gmail.com>
2017-09-22 17:27:53 +0300

    insertions

--- app/components/question-tile.js
+++ app/components/question-tile.js
@@ -1,5 +1,18 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
-  isAuthorShowing: false
+  isAuthorShowing: false,
+  actions: {
+    authorShow: function() {
+      this.set('isAuthorShowing', true);
+    },
+    authorHide: function() {
+      this.set('isAuthorShowing', false);
+    },
+    answer(question) {
+      if (confirm('Are you satisfied with our answer pal?')) {
+        this.sendAction('giveAnswer', question);
+      }
+    }
+  }
 });
