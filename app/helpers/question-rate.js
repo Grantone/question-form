c524b29ad7e66fe83832fb1a8fdf13b25fa034ff
Granton Mchana <gmchana254@gmail.com>
2017-09-30 18:33:48 +0300

    insertions

--- app/helpers/question-rate.js
+++ app/helpers/question-rate.js
@@ -1,7 +1,17 @@
 import Ember from 'ember';
 
-export function questionRate(params/*, hash*/) {
-  return params;
+export function questionRate(params) {
+  var questionPercentage = params[0].get('rate');
+
+  if (questionPercentage >= 200) {
+    return '$$$$';
+  } else if (questionPercentage >= 150) {
+    return '$$$';
+  } else if (questionPercentage >= 100) {
+    return '$$';
+  } else if (questionPercentage <= 99) {
+    return '$';
+  }
 }
 
 export default Ember.Helper.helper(questionRate);
