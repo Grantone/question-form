b74aebf493185a5b27ced4d0dbe7254431f60a03
Granton Mchana <gmchana254@gmail.com>
2017-10-02 14:13:59 +0300

    created a path

--- app/router.js
+++ app/router.js
@@ -8,7 +8,9 @@ const Router = Ember.Router.extend({
 Router.map(function() {
   this.route('about');
   this.route('contact');
-  this.route('question');
+  this.route('question', {
+    path: 'question/:question_id'
+  })
 });
 
 export default Router;
